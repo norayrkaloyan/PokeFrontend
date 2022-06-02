0aefc87bb23a1ff799e059f0984feb5bff160c8e
Norayr <norayrkaloyan@yahoo.com>
2022-06-03 00:59:45 +0200

    making loose or win on blackboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
+import "./App.css";
 //import Searchbar from './components/Searchbar';
-import Results from './components/Results';
-import { Routes, Route} from 'react-router-dom';
-import Fight from './components/Fight.js';
+import Results from "./components/Results";
+import { Routes, Route } from "react-router-dom";
+import Fight from "./components/Fight.js";
 
 function App() {
-
   return (
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Results />} />
-          <Route path="/:id" element={<Fight />} />
-        </Routes>
-      </div>
-    );
-  }
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Results />} />
+        <Route path="/:id" element={<Fight />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
--- src/components/Blackboard.js
+++ src/components/Blackboard.js
@@ -1,18 +1,119 @@
+import PokeBallButton from "./PokeBallButton.js";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
+const Blackboard = ({
+  firstPlayerHealth,
+  botPlayerHealth,
+  damage,
+  playerName,
+  botName,
+  attack,
+}) => {
+  const [newFirstPlayerHealth, setNewFirstPlayerHealth] = useState(100);
+  const [newBotPlayerHealth, setNewBotPlayerHealth] = useState(100);
+  const [playerDamage, setPlayerDamage] = useState();
+  const [botDamage, setBotDamage] = useState();
+  const [attacking, setAttacking] = useState();
+  const [attacker, setAttacker] = useState("");
+  const navigate = useNavigate();
 
-const Blackboard = () => {
+  useEffect(() => {
+    setNewFirstPlayerHealth(firstPlayerHealth);
+    setNewBotPlayerHealth(botPlayerHealth);
+    setAttacking(attack);
+    setPlayerDamage(damage);
+  }, [firstPlayerHealth, botPlayerHealth, damage, attack, attacker]);
+
+  console.log(attacking, attacker, botName, playerName);
+
+  console.log(attacking, attacker, botName, playerName, damage);
+
+  const newGame = () =>{
+    navigate("/")
+  }
+
   return (
     <>
-    <div className="contenteditable">
-        Pokemon Fight
-    <br/>
-    <ul>
-    <li>Test</li>
-    </ul>
-    <br/>
-    </div>
+      <div className="contenteditable">
+        <div className="attacker">
+          {attack === 1 ? (
+            <h2>
+              <b>{botName}</b> attacks{" "}
+            </h2>
+          ) : (
+            <h2>
+              <b>{playerName}</b> attacks
+            </h2>
+          )}
+        </div>
+        <div className="daten">
+          <div className="player-daten">
+            <h2>
+              <b>Player1:</b>
+              {playerName}
+            </h2>
+            {/* <h4>{newFirstPlayerHealth}</h4> */}
+            {newFirstPlayerHealth < 0 ? (
+              <h4>Health: 0</h4>
+            ) : (
+              <h4>Health: {newFirstPlayerHealth}</h4>
+            )}
+            {/* {attack === 1 ? <h2>{playerDamage}</h2> : <h2>{botDamage}</h2>} */}
+          </div>
+
+          <div className="bot-daten">
+            <h2>
+              <b>Player2:</b>
+              {botName}
+            </h2>
+            {/* <h4>{newBotPlayerHealth}</h4> */}
+            {newBotPlayerHealth < 0 ? (
+              <h4>Health: 0</h4>
+            ) : (
+              <h4>Health:{newBotPlayerHealth}</h4>
+            )}
+            {/* <h2>{botDamage}</h2> */}
+          </div>
+        </div>
+        <br />
+        <div className="won-loose">
+          <div>
+            {newBotPlayerHealth === 0 ? (
+              <h1 className="youwon">You have Won</h1>
+            ) : (
+              <h4></h4>
+            )}
+          </div>
+          <div>
+            {newBotPlayerHealth === 0 ? (
+              <button className="newGame" onClick={newGame}>New Game</button>
+            ) : (
+              <h4></h4>
+            )}
+          </div>
+          <div>
+            {newFirstPlayerHealth === 0 ? (
+              <h1>
+                {botName} has Won!!!
+              </h1>
+            ) : (
+              <h4></h4>
+            )}
+          </div>
+          <div>
+            {newFirstPlayerHealth === 0 ? (
+              <button className="newGame" onClick={newGame}>New Game</button>
+            ) : (
+              <h4></h4>
+            )}
+          </div>
+        </div>
+
+        <br />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Blackboard;
